5ff812e771aa2fd4055cfafc9c7150454a985f60
andy103 <andy.h@5gl.dev>
2023-11-13 21:32:25 +0700

    checkpoint add health function

--- lib/custom_code/actions/get_health_data.dart
+++ lib/custom_code/actions/get_health_data.dart
@@ -1,4 +1,7 @@
 // Automatic FlutterFlow imports
+import 'package:health/health.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'index.dart'; // Imports other custom actions
@@ -7,5 +10,40 @@ import 'package:flutter/material.dart';
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
 Future getHealthData() async {
-  // Add your function code here!
+  var types = [
+    HealthDataType.STEPS,
+    HealthDataType.HEART_RATE,
+
+    // HealthDataType.ACTIVE_ENERGY_BURNED
+    HealthDataType.SLEEP_SESSION
+  ];
+  HealthFactory health = HealthFactory(useHealthConnectIfAvailable: true);
+  var permissions = [types].map((e) => HealthDataAccess.READ).toList();
+  await Permission.activityRecognition.request();
+  await Permission.location.request();
+  var a;
+  try {
+    a = await health.requestAuthorization(types, permissions: permissions);
+  } catch (error) {
+    print("Exception in authorize: $error");
+  }
+
+  var now = DateTime.now();
+
+  // fetch health data from the last 24 hours
+
+  // request permissions to write steps and blood glucose
+  // get the number of steps for today
+  var midnight = DateTime(now.year, now.month, now.day);
+  var endDate = DateTime(now.year, now.month, now.day, 23, 59, 59);
+  int? steps = await health.getTotalStepsInInterval(midnight, endDate);
+  var heartRate = await health
+      .getHealthDataFromTypes(midnight, endDate, [HealthDataType.HEART_RATE]);
+  var sleep = await health.getHealthDataFromTypes(
+      midnight, endDate, [HealthDataType.SLEEP_SESSION]);
+  print('heart rate nè: => ' + heartRate[0].value.toString());
+  var ss= sleep[0].toJson()['value']['numericValue'];
+  print('Sleep nè: => ' + ss);
+  print('heart rate nè: => ' + steps.toString());
+  var d = 1;
 }
